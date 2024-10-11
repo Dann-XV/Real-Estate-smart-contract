efc6a6ec7b0a36720910ffe24f7064c704f774ee
Daniel Bamgbose <dharnielb15@gmail.com>
2024-10-11 07:15:53 +0100

    latest update to millow app

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -19,11 +19,22 @@ contract Escrow {
         require(msg.sender == seller, "Only seller can call this method");
         _;
     }
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "Only buyer can call this function");
+        _;
+        
+    }
+    modifier onlyInspector{
+        require(msg.sender == inspector, "Only inspector can call this function");
+        _; 
+    }
 
-    mapping (uint256 => bool) public isListed;
-    mapping (uint256 => uint256) public purchasePrice;
-    mapping (uint256 => uint256) public escrowAmount;
-    mapping (uint256 => address) public buyer; 
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer; 
+    mapping(uint256 => bool) public inspectionPassed; 
+    mapping(uint256 => mapping(address => bool)) public approval; 
 
     constructor (address _nftAddress, address payable _seller, address _inspector, address _lender){
         nftAddress = _nftAddress;
@@ -32,7 +43,7 @@ contract Escrow {
         lender = _lender;
     }
 
-    function list(uint256 _nftID, address _buyer, uint256 _purchasePrice, uint256 _escrowAmount )public{
+    function list(uint256 _nftID, address _buyer, uint256 _purchasePrice, uint256 _escrowAmount )public payable onlySeller{
         // Transfer NFT from seller to contract
         IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
 
@@ -40,5 +51,24 @@ contract Escrow {
         purchasePrice[_nftID] = _purchasePrice;
         escrowAmount[_nftID] = _escrowAmount;
         buyer[_nftID] = _buyer;
+    }
+
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID){
+        require(msg.value >= escrowAmount[_nftID]);
+        
+    }
+
+    function approveSale(uint256 _nftID) public{
+        approval[_nftID][msg.sender] = true;
+    }
+
+    receive() external payable {} 
+
+    function getBalance() public view  returns (uint256) {
+        return address(this).balance;    
+    }
+
+    function updateInspectionStatus(uint256 _nftID, bool _passed) public onlyInspector returns(bool) {
+        inspectionPassed[_nftID] = _passed;
     }
 }
