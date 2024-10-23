897b0244030340aec03bbceaa4104d516805d6d2
Daniel Bamgbose <dharnielb15@gmail.com>
2024-10-23 08:39:36 +0100

    added connect wallet feature to front end navbar

--- src/App.js
+++ src/App.js
@@ -15,12 +15,16 @@ import config from './config.json';
 
 function App() {
 
+  const [account, setAccount] = useState(null);
+
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    console.log(provider);
-    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
-    console.log(accounts);
-    // 3:18:10
+
+    window.ethereum.on('accountsChanged', async () =>{
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    })
   }
 
   useEffect(() =>{
@@ -29,13 +33,33 @@ function App() {
 
   return (
     <div>
+      <Navigation account = {account} setAccount={setAccount}/>
+      <Search/>
 
       <div className='cards__section'>
 
-        <h3>Welcome to Millow!</h3>
+        <h3>Homes For You</h3>
 
+        <hr />
+
+        <div className='cards'>
+          <div class ='card'>
+          <div className='card__image'>
+            <img src = '' alt="Home" />
+          </div>
+          <div className='card__info'>
+            <h4>1 ETH</h4>
+            <p>
+              <strong>4</strong> bds |
+              <strong>5</strong> ba |
+              <strong>250</strong> sqft
+            </p>
+            <p>123 Neverland</p>
+          </div>
+        </div>
+          
+        </div>
       </div>
-
     </div>
   );
 }
