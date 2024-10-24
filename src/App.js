9db252338937a498122797200b63ff548a468d7a
Daniel Bamgbose <dharnielb15@gmail.com>
2024-10-24 07:17:41 +0100

    more updates

--- src/App.js
+++ src/App.js
@@ -15,10 +15,20 @@ import config from './config.json';
 
 function App() {
 
+  const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider);
+    const totalSupply = await realEstate.totalSupply();
+    console.log(totalSupply);
+    
+    
 
     window.ethereum.on('accountsChanged', async () =>{
       const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
